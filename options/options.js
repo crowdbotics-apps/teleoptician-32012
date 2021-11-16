88a5ffdcb1bda78e543ce462f6e140d3a12c176a
Crowdbotics <team@crowdbotics.com>
2021-11-16 15:35:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "teleoptician_32012",
+  url: "https://teleoptician_32012.botics.co",
+  api: "https://teleoptician_32012.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
